c20dd7b65e10bea7f154e0227390ec5eeae78f15
pomcom <git@pomcom.email>
2023-01-20 11:12:47 +0100

    adjust error handling, declutch code

--- pkg/services/testRunnerService.go
+++ pkg/services/testRunnerService.go
@@ -1,26 +1,23 @@
 package services
 
 import (
+	"fmt"
+
 	"github.com/pomcom/bagoScan/pkg/core"
 	"github.com/pomcom/bagoScan/pkg/tools"
 	"github.com/pomcom/bagoScan/pkg/utils/config"
-	"github.com/spf13/viper"
 )
 
 type TestRunnerService struct {
 	configHandler config.ConfigHandler
+	config        config.Config
 	runner        core.TestRunner
 	fileHandler   core.Filehandler
 }
 
-// Debug read config...
+// no error handling for ReadConfig() needed, since it returns empty valid struct in err case
 func (service TestRunnerService) RunAllTools(targets []string) error {
-	config, err := service.configHandler.ReadConfig()
-	if err != nil {
-		return err
-	}
-
-	runner := core.NewTestRunner(config.ToolMap)
+	runner := core.NewTestRunner(service.config.ToolMap)
 	outputs := runner.Run(targets)
 
 	for _, output := range outputs {
@@ -34,35 +31,15 @@ func (service TestRunnerService) RunAllTools(targets []string) error {
 }
 
 func (service TestRunnerService) RunSingleTool(toolName string, targets []string) error {
-	config, err := service.configHandler.ReadConfig()
-	if err != nil {
-		// check if file doesn't exist
-		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			for _, target := range targets {
-				tool, err := service.configHandler.ReadSingleToolConfig(toolName)
-				if err != nil {
-					return err
-				}
-				singleToolMap := map[string]tools.Tool{toolName: tool}
-				runner := core.NewTestRunner(singleToolMap)
-				outputs := runner.Run([]string{target})
-
-				for _, output := range outputs {
-					fileName := output.ToolName + "-" + target + "-output.txt"
-					err := service.fileHandler.WriteToFile(fileName, output.Result)
-					if err != nil {
-						return err
-					}
-				}
-				return nil
-			}
-		}
-		return err
+	tool, ok := service.config.ToolMap[toolName]
+	if !ok {
+		return fmt.Errorf("tool not found: %s", toolName)
 	}
-	// create a new TestRunner with only the specified tool
-	singleToolMap := map[string]tools.Tool{toolName: config.ToolMap[toolName]}
+
+	singleToolMap := map[string]tools.Tool{toolName: tool}
 	runner := core.NewTestRunner(singleToolMap)
 	outputs := runner.Run(targets)
+	println("hi")
 
 	for _, output := range outputs {
 		fileName := output.ToolName + "-" + output.Target + "-output.txt"
@@ -72,14 +49,12 @@ func (service TestRunnerService) RunSingleTool(toolName string, targets []string
 		}
 	}
 	return nil
+
 }
 
 func NewTestRunnerService(configPath string) TestRunnerService {
 	configHandler := config.NewConfigHandler(configPath)
-	config, err := configHandler.ReadConfig()
-	if err != nil {
-		panic(err)
-	}
+	config := configHandler.ReadConfig()
 	fileHandler := core.NewFilehandler("")
-	return TestRunnerService{configHandler: configHandler, fileHandler: fileHandler, runner: core.NewTestRunner(config.ToolMap)}
+	return TestRunnerService{configHandler: configHandler, fileHandler: fileHandler, config: config}
 }
