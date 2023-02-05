5852c4c30e9c8cb905ccfa2a2b5fd25c02d8ebf3
pomcom <git@pomcom.email>
2023-02-05 12:10:39 +0100

    Adjusts parser

--- pkg/utils/reportingParser/reportingParser.go
+++ pkg/utils/reportingParser/reportingParser.go
@@ -1,18 +1,20 @@
 package reportingparser
 
 import (
+	"bufio"
 	"encoding/csv"
 	"fmt"
 	"os"
+	"regexp"
 )
 
 func ReportingParser() {
-	file, err := os.Open("output/raw/combined-output.txt")
+	outputFile, err := os.Open("output/raw/combined-output.txt")
 	if err != nil {
 		fmt.Println("Error opening file:", err)
 		return
 	}
-	defer file.Close()
+	defer outputFile.Close()
 
 	// Create a new CSV file
 	csvFile, err := os.Create("output_nessus_format.csv")
@@ -57,17 +59,74 @@ func ReportingParser() {
 
 	err = writer.Write(header)
 	if err != nil {
-		fmt.Println("Error writing headers:", err)
+		fmt.Println("Error writing header to CSV file:", err)
 		return
+	}
+
+	//Regex patterns sqlmap - check for valid SQL injection
+
+	// hostRegex := regexp.MustCompile(`GET (.*?)/rest`)
+	sqlInjectionRegex := regexp.MustCompile(`do you want to exploit this SQL injection\? \[Y/n\] Y`)
+	outputRegex := regexp.MustCompile(`\[INFO\] retrieved: (.*?)\n`)
+
+	// injectionRegex := regexp.MustCompile(`(\d+)\s.*\n.*(GET)\n.*(\w+-based.*)\n.*(\w+.*)\n`)
+
+	var host string
+	var pluginOutput string
+	var sqlInjectionFound bool
+
+	scanner := bufio.NewScanner(outputFile)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if sqlInjectionRegex.MatchString(line) {
+			sqlInjectionFound = true
+			// host = hostRegex.FindStringSubmatch(line)[1]
+		} else if sqlInjectionFound && outputRegex.MatchString(line) {
+			pluginOutput = outputRegex.FindStringSubmatch(line)[1]
+		}
 	}
 
 	// Write the SQL injection information to the CSV file
-	data := []string{"123456", "", "", "High", "localhost", "HTTP", "8080", "SQL Injection Vulnerability", "A SQL injection vulnerability has been found in the application.", "The application is vulnerable to SQL injection attacks, which can allow an attacker to execute arbitrary SQL commands.", "Upgrade the application to the latest version to fix the vulnerability.", "", "SQL Injection: http://localhost:8080/rest/products/search?q="}
-	err = writer.Write(data)
-	if err != nil {
-		fmt.Println("Error writing data:", err)
-		return
+	data := []string{
+		"123456",
+		"",
+		"",
+		"High",
+		host,
+		"HTTP",
+		"8080",
+		"SQL Injection Vulnerability",
+		"A SQL injection vulnerability has been discovered in this application.",
+		"The application is vulnerable to SQL injection attacks, which can allow an attacker to execute arbitrary SQL commands on the underlying database. This can result in the theft of sensitive data, such as user credentials and other confidential information.",
+		"Apply patches or upgrades to the application to eliminate the vulnerability.",
+		"",
+		pluginOutput,
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
 	}
 
+	if sqlInjectionFound {
+		// Write the data to the CSV file
+		err = writer.Write(data)
+		if err != nil {
+			fmt.Println("Error writing data to CSV file:", err)
+			return
+		}
+	}
+
+	defer csvFile.Close()
+	defer writer.Flush()
 	fmt.Println("Data written to Nessus format CSV file successfully")
+
 }
